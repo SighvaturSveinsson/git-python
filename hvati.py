22690629a46a76da2601b7654b0c36ece2f84f9d
Sighvatur Sveinsson <sighvatursv373@nemi.tskoli.is>
2017-01-25 09:16:05 +0000

    daemi 3

--- hvati.py
+++ hvati.py
@@ -13,3 +13,14 @@ print("Margfeldi talnanna er", margfeldi)
 fornafn = input("Sláðu inn fornafn ")
 eftirnafn = input("Sláðu inn eftirnafn ")
 print("Halló", fornafn, eftirnafn)
+
+#Dæmi 3
+texti = input("Sláðu inn texta ")
+hastafir = 0
+lagstafir = 0
+for i in texti:
+    if i .isupper() and i .isalpha():
+        hastafir += 1
+    elif i .islower() and i .isalpha():
+        lagstafir += 1
+print("Í þessum texta eru", hastafir, "hástafir,", lagstafir, "lágstafir")
