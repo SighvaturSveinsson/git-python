99d934d0e341edb976b691bad88c2b20d5d5d883
Sighvatur Sveinsson <sighvatursv373@nemi.tskoli.is>
2017-01-25 09:13:45 +0000

    daemi 2

--- hvati.py
+++ hvati.py
@@ -1,11 +1,15 @@
 #Python verkefni
 #Sighvatur Sveinsson
 
-tala1 = int(input("Sláðu inn tölu"))
-tala2 = int(input("Sláðu inn tölu"))
+#Dæmi 1
+tala1 = int(input("Sláðu inn tölu "))
+tala2 = int(input("Sláðu inn tölu "))
 summa = tala1 + tala2
 margfeldi = tala1 * tala2
+print("Summa talnanna er", summa)
+print("Margfeldi talnanna er", margfeldi)
 
-fornafn = input("Sláðu inn fornafn")
-eftirnafn = input("Sláðu inn eftirnafn")
+#Dæmi 2
+fornafn = input("Sláðu inn fornafn ")
+eftirnafn = input("Sláðu inn eftirnafn ")
 print("Halló", fornafn, eftirnafn)
